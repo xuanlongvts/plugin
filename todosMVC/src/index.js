d93b9c83e846ebea86074eb43293052f95ba7ed8
long.le <xuanlongvts@gmail.com>
2017-06-04 20:23:36 +0700

    updateLog

--- todosMVC/src/index.js
+++ todosMVC/src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
-import createLogger from 'redux-logger';
+import {createLogger}from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import reducer from './reducers/index';
 
-const store = createStore(reducer);
+//const store = createStore(reducer);
+
+const store = createStore(reducer, {}, compose(
+	applyMiddleware(createLogger()),
+	window.devToolsExtension?window.devToolsExtension():f=>f
+))
 
 ReactDOM.render(
 	<Provider store={store}>
