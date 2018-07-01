e14ac8ce2b328f463ff861b486467188d9cb09d3
long.le <xuanlongvts@gmail.com>
2018-07-01 12:18:09 +0700

    finish test

--- test/series/src/listComponent/login.test.js
+++ test/series/src/listComponent/login.test.js
@@ -3,8 +3,11 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 import { shallow, mount, render } from 'enzyme';
+import TestRenderer from 'react-test-renderer';
 
 import Login from './login';
+
+// https://hackernoon.com/implementing-basic-component-tests-using-jest-and-enzyme-d1d8788d627a
 
 describe('Login component', () => {
     const wrapper = shallow(<Login />);
@@ -26,27 +29,34 @@ describe('Login component', () => {
 
     describe('Email input', () => {
         it('should respond to change event and change the state of the Login component', () => {
+            const email = 'abc@gmail.com';
             wrapper.find('#email').simulate('change', {
                 target: {
                     name: 'email',
-                    value: 'abc@gmail.com'
+                    value: email
                 }
             });
 
-            expect(wrapper.state('email')).toEqual('abc@gmail.com');
+            expect(wrapper.state('email')).toEqual(email);
         });
     });
 
     describe('Password input', () => {
         it('should respond to change event and change the state of the Login component', () => {
+            const valuePass = 'cats';
             wrapper.find('#password').simulate('change', {
                 target: {
                     name: 'password',
-                    value: 'cats'
+                    value: valuePass
                 }
             });
 
-            expect(wrapper.state('password')).toEqual('cats');
+            expect(wrapper.state('password')).toEqual(valuePass);
         });
+    });
+
+    it('render correctly', () => {
+        const tree = TestRenderer.create(<Login />).toJSON();
+        expect(tree).toMatchSnapshot();
     });
 });
